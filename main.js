182118ab08d79103cce3cf63c3523ed455be5774
TheCalculus <thecalculus@users.noreply.github.com>
2023-07-16 12:42:03 +1000

    major rewrite. switched from threejs to vanilla

--- main.js
+++ main.js
@@ -1,32 +1,115 @@
-import * as THREE from "three";
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-const fov = 95;
-const aspect = window.innerWidth / window.innerHeight;
-const near = 0.1;
-const far = 1000;
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+let particles = [];
 
-const renderer = new THREE.WebGLRenderer();
+const PARTICLE_COUNT = 1000;
+const MASS_MAX = 1000;
+const G = 10;
+const TIME_STEP = 0.001;
+const MIN_DISTANCE_SQ = 100;
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+let clietnX, clientY;
 
-const sphere = {
-    geometry: new THREE.SphereGeometry(15),
-    material: new THREE.MeshBasicMaterial({ color: 0xffff00 }),
-    object: null,
-};
-
-sphere.object = new THREE.Mesh(sphere.geometry, sphere.material);
-scene.add(sphere.object);
-
-camera.position.z = 40;
-
-function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
+function randomRange(min, max) {
+    return Math.random() * (max - min) + min;
 }
 
-animate();
+function initializeParticles() {
+    for (let i = 0; i < PARTICLE_COUNT; i++) {
+        let x = randomRange(0, canvas.width);
+        let y = randomRange(0, canvas.height);
+
+        let mass = Math.floor(Math.random() * MASS_MAX);
+        let radius = mass ** 0.1;
+
+        particles.push({
+            x: x,
+            y: y,
+            mass: mass,
+            radius: radius,
+            vx: 0,
+            vy: 0,
+        });
+    }
+}
+
+document.addEventListener("mousemove", (event) => {
+    clientX = event.clientX;
+    clientY = event.clientY;
+});
+
+initializeParticles();
+
+function calculateForces() {
+    particles.forEach((particle, indexa) => {
+        let ax = 0;
+        let ay = 0;
+
+        particles.forEach((other, indexb) => {
+            if (particle !== other) {
+                let dx = other.x - particle.x;
+                let dy = other.y - particle.y;
+                let distance = Math.sqrt(dx * dx + dy * dy);
+
+                if (distance > MIN_DISTANCE_SQ) {
+                    let force =
+                        (G * (particle.mass * other.mass)) / distance ** 2;
+                    force /= Math.sqrt(particle.mass);
+
+                    ax += force * (dx / distance);
+                    ay += force * (dy / distance);
+                }
+            }
+        });
+
+        particle.ax = ax;
+        particle.ay = ay;
+    });
+}
+
+function updateParticles() {
+    particles.forEach((particle, index) => {
+        particle.vx += particle.ax * TIME_STEP;
+        particle.vy += particle.ay * TIME_STEP;
+        particle.x += particle.vx * TIME_STEP;
+        particle.y += particle.vy * TIME_STEP;
+
+        if (
+            particle.x < -particle.radius ||
+            particle.x > canvas.width + particle.radius ||
+            particle.y < -particle.radius ||
+            particle.y > canvas.height + particle.radius
+        ) {
+            particles.splice(index, 1);
+        }
+    });
+}
+
+ctx.fillStyle = "white";
+
+const MAX_ITERS = 10000;
+let iterations = 0;
+
+function draw() {
+    iterations += 1;
+    if (iterations >= MAX_ITERS) return;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    calculateForces();
+    updateParticles();
+
+    particles.forEach((particle) => {
+        ctx.beginPath();
+        ctx.arc(particle.x, particle.y, particle.radius, 0, 2 * Math.PI);
+        ctx.fill();
+    });
+
+    requestAnimationFrame(draw);
+}
+
+draw();
