94cf049e13600364d2a92df30fb352cf89e81590
TheCalculus <thecalculus@users.noreply.github.com>
2023-07-17 10:56:07 +1000

    css

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 let particles = [];
-const PARTICLE_COUNT = 1000;
-const MASS_MAX = 1000;
-const G = 10;
-const TIME_STEP = 0.005;
+
+const PARTICLE_COUNT = Math.floor(Math.random() * 1000);
+const G = 6.674 * 10e-3;
+const TIME_STEP = 0.00005;
 const MIN_DISTANCE_SQ = 100 ** 2;
 
 function randomRange(min, max) {
@@ -20,8 +20,8 @@ function initializeParticles() {
         let x = randomRange(0, canvas.width);
         let y = randomRange(0, canvas.height);
 
-        let mass = Math.floor(Math.random() * MASS_MAX);
-        let radius = mass ** 0.1;
+        let mass = 1e7;
+        let radius = mass ** 0.05;
 
         particles.push({
             x: x,
@@ -36,33 +36,33 @@ function initializeParticles() {
 
 function calculateForces() {
     const len = particles.length;
-    particles.forEach((particle, indexa) => {
+    for (let indexa = 0; indexa < len; indexa++) {
+        let particle = particles[indexa];
         let ax = 0;
         let ay = 0;
 
-        for (let indexb = 0; indexb < len; indexb++) {
-            if (indexa === indexb) continue;
+        for (let indexb = indexa + 1; indexb < len; indexb++) {
             let other = particles[indexb];
 
             let dx = other.x - particle.x;
             let dy = other.y - particle.y;
 
             let distanceSquared = dx ** 2 + dy ** 2;
-            let distance_c = Math.sqrt(dx ** 2 + dy ** 2);
+            let distance_c = Math.sqrt(distanceSquared);
 
             if (distanceSquared > MIN_DISTANCE_SQ) {
                 let force =
                     (G * (particle.mass * other.mass)) / distanceSquared;
                 force /= Math.sqrt(particle.mass);
 
-                ax += (force * dx) / Math.sqrt(distanceSquared);
-                ay += (force * dy) / Math.sqrt(distanceSquared);
+                ax += (force * dx) / distance_c;
+                ay += (force * dy) / distance_c;
             }
         }
 
         particle.ax = ax;
         particle.ay = ay;
-    });
+    }
 }
 
 function updateParticles() {
@@ -102,3 +102,10 @@ function draw() {
 
 initializeParticles();
 draw();
+
+function resizeCanvas() {
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+}
+
+addEventListener("resize", resizeCanvas);
