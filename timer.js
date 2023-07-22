d4e4983487a117090e1e0d8f7ae3c87dca3c0f72
TheCalculus <thecalculus@users.noreply.github.com>
2023-07-22 12:07:09 +1000

    begin about

---
+++ timer.js
@@ -0,0 +1,22 @@
+let timer = document.getElementById("timer");
+
+let initial = new Date(2020, 3, 20);
+let delta = (new Date() - initial);
+
+// an interesting question: which series of divisions is more performant?
+// the many, smaller divisions or the singular, humongous division? in practice,
+// this difference is probably negligible so I will retain the singular division
+
+// delta /= 1000;
+// delta /= 60;
+// delta /= 60;
+// delta /= 24;
+// delta /= 365;
+
+// 1000 * 60 * 60 * 24 * 365 = 31536000000
+delta /= 31536000000;
+
+setInterval(() => {
+    timer.innerHTML = delta;
+    delta += 3.1709792e-10; // 10 / 31536000000
+}, 10);
